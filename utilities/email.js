10978d763b1cea04d17ba81b5afac35c06d0b59a
Steven Omegna <skomegna@uw.edu>
2021-10-27 12:29:09 -0700

    Added email.js to utilities.

--- utilities/email.js
+++ utilities/email.js
@@ -1,21 +1,49 @@
-let sendEmail = (sender, receiver, subject, message) => {
-    //research nodemailer for sending email from node.
-    // https://nodemailer.com/about/
-    // https://www.w3schools.com/nodejs/nodejs_email.asp
-    //create a burner gmail account 
-    //make sure you add the password to the environmental variables
-    //similar to the DATABASE_URL and PHISH_DOT_NET_KEY (later section of the lab)
+/*
+ * Visit these links for more information on nodemailer
+ * https://nodemailer.com/about/
+ * https://www.w3schools.com/nodejs/nodejs_email.asp 
+ */
 
-    //fake sending an email for now. Post a message to logs. 
-    console.log("*********************************************************")
-    console.log('To: ' + receiver)
-    console.log('From: ' + sender)
-    console.log('Subject: ' + subject)
-    console.log("_________________________________________________________")
-    console.log(message)
-    console.log("*********************************************************")
+// make sure you add the password to the environmental variables
+// similar to the DATABASE_URL and PHISH_DOT_NET_KEY (later section of the lab)
 
+// require nodemailer for sending the email
+const nodemailer = require('nodemailer');
+
+// the password stored in a secure .env file
+const EMAIL_PASS = process.env.BURNER_EMAIL_PASS;
+
+// the email that will be used to send emails
+const EMAIL = 'tcss450.1.no.reply@gmail.com'
+
+
+// sends an email to the given recieverEmail with the
+// given subject name and message.
+const sendEmail = (receiverEmail, subject, message) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: EMAIL,
+          pass: EMAIL_PASS
+        }
+    });
+
+    var mailOptions = {
+        from: EMAIL,
+        to: receiverEmail,
+        subject: subject,
+        text: message
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+    });
 }
+
 
 module.exports = { 
     sendEmail
