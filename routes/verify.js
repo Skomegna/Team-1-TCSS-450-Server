32d81ed0f6a9926f6122dcf12e78a787350615a1
msanyok <msanyoks@yandex.ua>
2021-11-07 18:08:46 -0800

    merged from master

---
+++ routes/verify.js
@@ -0,0 +1,108 @@
+//express is the framework we're going to use to handle requests
+const express = require('express');
+
+//Create a new instance of express router
+var router = express.Router();
+
+//Access the connection to Heroku Database
+const pool = require('../utilities').pool;
+
+//validation tools
+const validation = require('../utilities').validation;
+const deleteVerificationCodeRow = require('../utilities').validation.deleteVerificationCodeRow;
+let isStringProvided = validation.isStringProvided;
+
+/**
+ * @api {post} /verify Request an message echo with a parameter 
+ * @apiName PostVerify
+ * @apiGroup Verify
+ * 
+ * @apiParam {String} email Account email
+ * 
+ * @apiParam {Int} code 6 Digit Verification Code 
+ * 
+ * * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 201 OK
+ *     {
+ *       "success": true,
+ *       "message": "Thank you for verifying",
+ *     }
+ * 
+ * @apiSuccess {String} message "Thank you for verifying"
+ * 
+ * @apiSuccess {boolean} success true when the codes match.
+ * 
+ * @apiError (400: Missing Parameters) {String} message "Missing required information"
+ * 
+ * @apiError (400: Incorrect Account) {String} message "Incorrect account information"
+ * 
+ * @apiError (400: Incorrect Code) {String} message "Incorrect Code"
+ * 
+ * @apiError (400: Wrong Email) {String} message "Please check credentials and try again."
+ * 
+ * @apiError (400: Other Error) {String} message "other error, see detail"
+ * @apiError (400: Other Error) {String} detail  Information about the error
+ */ 
+router.post("/", (request, response, next) => {
+    if (isStringProvided(request.body.email) && isStringProvided(request.body.code)) {
+        next();
+    } else {
+        response.status(400).send({
+            message: "Missing required information"
+        });
+    };
+}, (request, response, next) => {
+    const email = (request.body.email).toLowerCase();
+    const userCode = request.body.code;
+    let theQuery = "SELECT Code FROM VerificationCode WHERE Email=$1";
+    let theValues = [email];
+    pool.query(theQuery, theValues)
+        .then (result => {
+            if (result.rows[0].code == userCode) {
+                next();
+            } else if (result.rows == 0) {
+                response.status(400).send({
+                    message: "Incorrect account information"
+                });   
+            } else {
+                response.status(400).send({
+                    message: "Incorrect Code"
+                });
+            };
+        })
+        .catch((error) => {
+            if (error.detail == undefined) {
+                    response.status(400).send({
+                        message: "Please check credentials and try again."
+                    });
+                } else {
+                    response.status(400).send({
+                        message: "other error, see detail",
+                        detail: error.detail     
+                    });
+                };
+        });
+
+}, (request, response, next) => {
+    const email = (request.body.email).toLowerCase();
+    let theQuery = "UPDATE Members SET Verification='1' WHERE email=$1"; 
+    let theValues = [email];
+    pool.query(theQuery, theValues)
+        .then (result => {
+            next();
+        })
+        .catch((err) => {
+            //log the error
+            console.log(err.stack);
+            response.status(400).send({
+                message: err.detail
+            });
+        });
+}, deleteVerificationCodeRow, (request, response) => {
+    response.status(201).send({
+        success: true,
+        message: "Thank you for verifying"
+    });
+});
+
+module.exports = router;
---
+++ utilities/email.js
@@ -0,0 +1,50 @@
+/*
+ * Visit these links for more information on nodemailer
+ * https://nodemailer.com/about/
+ * https://www.w3schools.com/nodejs/nodejs_email.asp 
+ */
+
+// make sure you add the password to the environmental variables
+// similar to the DATABASE_URL and PHISH_DOT_NET_KEY (later section of the lab)
+
+// require nodemailer for sending the email
+const nodemailer = require('nodemailer');
+
+// the password stored in a secure .env file
+const EMAIL_PASS = process.env.BURNER_EMAIL_PASS;
+
+// the email that will be used to send emails
+const EMAIL = 'tcss450.1.no.reply@gmail.com';
+
+
+// sends an email to the given recieverEmail with the
+// given subject name and message.
+const sendEmail = (receiverEmail, subject, message) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: EMAIL,
+          pass: EMAIL_PASS
+        }
+    });
+
+    var mailOptions = {
+        from: EMAIL,
+        to: receiverEmail,
+        subject: subject,
+        text: message
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+    });
+};
+
+
+module.exports = { 
+    sendEmail
+};
